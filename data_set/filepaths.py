ddc6f4b4e04803ea66fec5315e80b277a1ad323f
rex <547102444@qq.com>
2020-12-14 23:00:58 +0800

    fb15k-237id

---
+++ data_set/filepaths.py
@@ -0,0 +1,5 @@
+import os
+#https://www.microsoft.com/en-us/download/details.aspx?id=52312
+#https://www.aclweb.org/anthology/W15-4007.pdf
+class FB15K_237():
+    base_path = os.path.join(os.path.split(os.path.realpath(__file__))[0], './FB15K-237')
